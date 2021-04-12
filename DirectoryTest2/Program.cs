2c782219956e82add0671c0f91fdf91c95762460
AnteUjcic <58471899+AnteUjcic@users.noreply.github.com>
2021-04-12 09:17:33 +0200

    Add files via upload

---
+++ DirectoryTest2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+
+
+namespace DirectoryTest2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string putanja = "$My Documents/ testNiOP /",ime,prezime,godrod,email;
+
+            Console.WriteLine("ime: ");
+            ime=Console.ReadLine();
+            Console.WriteLine("prezime: ");
+            prezime = Console.ReadLine();
+            Console.WriteLine("god_rodenja: ");
+            godrod = Console.ReadLine();
+            Console.WriteLine("E-mail: ");
+            email = Console.ReadLine();
+
+            addRecord(ime, prezime, godrod, email,putanja);
+           
+
+        
+            Console.ReadKey();
+
+        }
+        public static void addRecord(string ime, string prezime, string godrod, string email,string putanja)
+        {
+            try
+            {
+                using (System.IO.StreamWriter file = new System.IO.StreamWriter(putanja))
+                {
+
+                   object p = File.WriteLine(ime + "," + prezime + "," + godrod + "," + email);
+                }
+                
+            
+            }
+            catch (Exception ex)
+            {
+                throw new ApplicationException("OOOOPS ",ex );
+            }
+        }
+    }
+}
+ 
